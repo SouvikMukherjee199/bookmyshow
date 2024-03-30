399f767a2532c66667b2fc5b3e904aaa11731ce5
SouvikMukherjee199 <svkmukherjee1@gmail.com>
2024-03-30 23:50:45 +0530

    Updated the movie page

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -2,6 +2,31 @@
 import React from "react";
 import MovieHero from "../components/MovieHero/MovieHero.component";
 import { BsFillCameraReelsFill } from "react-icons/bs";
+
+
+const launchRazorPay = () =>{
+
+let options = {
+//careate an object called options with key-value pairs 
+key : "rzp_test_8wWHnjXlqKMbay" , //passing the Razorpay test key from the website
+amount : 500 , //or create an array of amounts or make a function to dynamically render the rupee values
+currency : "INR" ,
+name : "Book My Show Clone" ,
+description : "Movie Purchase on Rental" ,
+image : "https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" ,
+
+handler : () => {
+alert("Payment done");
+//prompts on getting the payment done
+},
+
+theme : { color:"#c4242d"}
+};
+
+let rzp = new window.Razorpay (options);//since the script of Razorpay is a part of html & a part of the new window(parent document following the DOM[Document Object Model] ) we access the Razorpay function 
+rzp.open();//this will open the new window when the launchrazorPay function gets triggered
+}
+
 const Movie = () => {
   return (
     <>
@@ -38,7 +63,13 @@ const Movie = () => {
         </div>
         </div>  
        </div>
-      
+
+       <button onClick={launchRazorPay} class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+       Book tickets
+       </button>
+
+
+
       </div>
 
      
